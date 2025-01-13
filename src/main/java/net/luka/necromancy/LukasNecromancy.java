2c784daea3b97c1075c6c0db1e7b6e911eb542bc
Luka <luka.talevski.hn@gmail.com>
2025-01-13 22:27:54 +0800

    Initial Commit

---
+++ src/main/java/net/luka/necromancy/LukasNecromancy.java
@@ -0,0 +1,17 @@
+package net.luka.necromancy;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.server.MinecraftServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LukasNecromancy implements ModInitializer {
+	public static final String MOD_ID = "lukas-necromancy";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/luka/necromancy/NecromancyClient.java
@@ -0,0 +1,10 @@
+package net.luka.necromancy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NecromancyClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
